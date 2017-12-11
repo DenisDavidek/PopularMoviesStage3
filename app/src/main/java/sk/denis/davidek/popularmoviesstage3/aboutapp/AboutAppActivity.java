295e49d13dce90d0dcfac076136e216061f7377b
Denis Dávidek <denis.davidek@gmail.com>
2017-12-11 19:55:28 +0100

    feat: Added an url link into get other SW products section
    Added a method which either opens G+ or a web page.
    
    Plus added connection to match the MVP pattern.

--- app/src/main/java/sk/denis/davidek/popularmoviesstage3/aboutapp/AboutAppActivity.java
+++ app/src/main/java/sk/denis/davidek/popularmoviesstage3/aboutapp/AboutAppActivity.java
@@ -1,5 +1,6 @@
 package sk.denis.davidek.popularmoviesstage3.aboutapp;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.widget.ImageView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import sk.denis.davidek.popularmoviesstage3.R;
 
 public class AboutAppActivity extends AppCompatActivity implements AboutAppContract.View {
@@ -17,6 +19,12 @@ public class AboutAppActivity extends AppCompatActivity implements AboutAppContr
 
     @BindView(R.id.iv_tmdb_logo)
     ImageView tmdbLogoImageView;
+
+    @OnClick(R.id.tv_get_uvplayer)
+    public void getProductsClicked(){
+
+        openGPlus("communities/109333228263928350527");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +38,20 @@ public class AboutAppActivity extends AppCompatActivity implements AboutAppContr
                 aboutAppPresenter.loadUrl();
             }
         });
+
+
+    }
+
+    public void openGPlus(String profile) {
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setClassName("com.google.android.apps.plus",
+                    "com.google.android.apps.plus.phone.UrlGatewayActivity");
+            intent.putExtra("customAppUri", profile);
+            startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://plus.google.com/" + profile)));
+        }
     }
 
     @Override
